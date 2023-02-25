f7369134010bb9604f4e7bf8cdc3c196a367c9f8
zohaib3249 <zohaib3249@gmail.com>
2023-02-25 12:15:00 +0500

    add optional fields

--- cynthia_app/models.py
+++ cynthia_app/models.py
@@ -8,7 +8,7 @@ class Features(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     name = models.CharField(max_length=30)
     state = models.CharField(max_length=30)
-    estimate_wd = models.FloatField()
+    estimate_wd = models.FloatField(null=True,blank=True)
     comment = models.CharField(max_length=500)
 
     def __str__(self):
@@ -20,8 +20,8 @@ class Member(models.Model):
     member_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     arrival_date = models.DateField(null=False)
-    leave_date = models.DateField()
-    comment = models.CharField(max_length=500)
+    leave_date = models.DateField(null=True,blank=True)
+    comment = models.CharField(max_length=500,null=True,blank=True)
 
     def __str__(self):
         return self.name
